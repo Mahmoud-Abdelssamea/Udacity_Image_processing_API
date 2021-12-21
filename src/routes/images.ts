c2d5d4ede1f6b0fb7e4f5d42825f5a6e8d958c1c
mahmoud-awwad <mabdelssamea@gmail.com>
2021-12-21 05:11:40 +0300

    create routes folder and inside create endpoint /convert

---
+++ src/routes/images.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { Response, Request, NextFunction } from "express";
+
+const router = Router();
+
+router.get("convert", (req, res, next) => {
+  const imageName = req.query.filename;
+  const width = req.query.width;
+  const hieght = req.query.hieght;
+  console.log(imageName, width, hieght);
+});
+
+export default router;
