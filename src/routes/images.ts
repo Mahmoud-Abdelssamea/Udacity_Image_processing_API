0cf8bb05f2a32eda035f7154f7802e7e8f2a6df7
mahmoud-awwad <mabdelssamea@gmail.com>
2021-12-21 05:42:03 +0300

    solve / missing problem

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("convert", (req, res, next) => {
+router.get("/convert/", (req, res, next) => {
   const imageName = req.query.filename;
   const width = req.query.width;
   const hieght = req.query.hieght;
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import convertRouter from "./routes/images";
 dotenv.config();
 const app = express();
 
-app.use("api/", convertRouter);
+app.use("/api", convertRouter);
 
 const port = process.env.PORT || 3000;
 
