f176c45c6db943423453773fa62c807a641344a4
mahmoud-awwad <mabdelssamea@gmail.com>
2021-12-24 21:21:49 +0300

    best practise adjust and formating for file

--- dist/routes/images.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const util_1 = require("../util");
-const router = (0, express_1.Router)();
-router.get("/convert/", util_1.resizingHandler);
-exports.default = router;
